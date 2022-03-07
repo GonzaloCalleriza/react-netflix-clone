19f2006dd6e6775818db95e7c06f1fe9e811da3d
gonzalogonzalo <calleriza.gonzalo@gmail.com>
2022-03-07 14:22:24 +0100

    Created sections and connected them with api. Working on movies

--- src/App.js
+++ src/App.js
@@ -1,9 +1,32 @@
 import './App.css';
+import { useState, useEffect } from 'react';
+import Section from './components/Section';
+
+
 
 function App() {
+  
+  const [genres, setGenres] = useState(null);
+  
+  const fetchData = async () => {
+    const url = "/.netlify/functions/getGenres"
+    const response = await fetch(url)
+
+    const responseBody = await response.json()
+
+    setGenres(responseBody.data.reference_list.values);
+  }
+
+  useEffect(() => {
+    fetchData()
+  }, [])
+
   return (
     <div >
-      Hello
+      {genres &&  genres.map((genre) => (
+        <Section genre={genre} key={genre._id}/>       
+      ))}
+      
     </div>
   );
 }
---
+++ src/components/Section.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Section = ({ genre }) => {
+    return (
+        <div>
+            <h1>{genre}</h1>
+        </div>
+    );
+}
+
+export default Section;
